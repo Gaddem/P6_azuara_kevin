95bf97ab2212f4bda84a36535fbf80c773fb026c
lmk-kevin <kevin@lmksystem.fr>
2022-09-15 02:07:52 +0200

    Finition page accueil + transmission données en GET par l'url

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,34 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait ,city ,country, tagline, price, id} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const h3 = document.createElement( 'h3' );
+        const h4 = document.createElement( 'h4' );
+        const h5 = document.createElement( 'h5' );
+
+        article.setAttribute("id", id)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt",name);
+        img.setAttribute("aria-label",name);
+        h3.textContent = city+", "+ country;
         h2.textContent = name;
+        h4.textContent = tagline;
+        h5.textContent = price+"€/jour";
+
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(h5);
+
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM, id }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,26 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+async function getInfosPhotographer(id_photographer) {
+    // Penser à remplacer par les données récupérées dans le json
+        const test = await fetch('../../data/photographers.json')
+        .then((response) => response.json())
+        .then((json) =>{
+             let arrayOfOncePhotographer = json.photographers.filter(photographer => photographer.id == id_photographer);
+             console.log(arrayOfOncePhotographer[0]);
+            return arrayOfOncePhotographer[0];
+            });
+
+        return test;
+}
+
+
+
+
+async function init() {
+
+    let params = (new URL(document.location)).searchParams;
+    let id_photographer = params.get('id');
+    const photographerInfo = await getInfosPhotographer(id_photographer);
+};
+
+init();
